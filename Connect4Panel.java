09e39d84e92d6e654110b2f2da70bc85d7d8e3a6
Vivek Chandy <chandyv05@gmail.com>
2024-05-19 22:43:32 -0400

    finished game

--- Connect4Panel.java
+++ Connect4Panel.java
@@ -7,11 +7,12 @@ public class Connect4Panel extends JPanel {
     int[][] board;
     HashMap<Integer,Color> colors;
     boolean p1turn;
+    boolean gameOver;
 
     public Connect4Panel(){
         board = new int[6][7];
-        for(int[] row:board)
-            System.out.println(Arrays.toString(row));
+        for(int[] r:board)
+            System.out.println(Arrays.toString(r));
 
         colors = new HashMap<>();
         colors.put(1, new Color(255,0,0));
@@ -19,15 +20,53 @@ public class Connect4Panel extends JPanel {
         colors.put(-1, new Color(200,200,0));
 
         p1turn = true;
-        //test
+        gameOver = false;
     }
 
     public void playMove(int c){
+        if(gameOver) return;
+
         int r = 0;
         while(r<5&&board[r+1][c]==0)
             r++;
         board[r][c] = p1turn ? 1 : -1;
         p1turn = !p1turn;
+
+        if(checkWinner()!=0) gameOver = true;
+
+        if(!p1turn) AImove();
+    }
+
+    public int checkWinner() {
+        for (int r = 0; r < 6; r++)
+            for (int c = 0; c < 4; c++)
+                for(int i=-1;i<2;i+=2)
+                    if (board[r][c] == i && board[r][c + 1] == i && board[r][c + 2] == i && board[r][c + 3] == i) 
+                        return i;
+    
+        for (int c = 0; c < 7; c++)
+            for (int r = 0; r < 3; r++)
+                for(int i=-1;i<2;i+=2)
+                    if (board[r][c] == i && board[r + 1][c] == i && board[r + 2][c] == i && board[r + 3][c] == i)
+                        return i;
+
+        for (int r = 0; r < 3; r++) 
+            for (int c = 0; c < 4; c++)
+                for(int i=-1;i<2;i+=2)
+                    if (board[r][c] == i && board[r + 1][c + 1] == i && board[r + 2][c + 2] == i && board[r + 3][c + 3] == i) 
+                        return i;
+       
+        for (int r = 3; r < 6; r++)
+            for (int c = 0; c < 4; c++)
+                for(int i=-1;i<2;i+=2)
+                    if (board[r][c] == i && board[r - 1][c + 1] == i && board[r - 2][c + 2] == i && board[r - 3][c + 3] == i)
+                        return i;
+
+        return 0;
+    }
+
+    public void AImove(){
+        playMove(3);
     }
 
     public void paintComponent(Graphics g){
